059edb56cbc10ba0dab6f84371d9199c45b20bd4
Ombline Brunet <bonbonblond@laposte.net>
2023-10-31 14:45:28 +0100

    J'ai organisé le fichier.

--- code_1.py
+++ code_1.py
@@ -1,5 +1,27 @@
+## importations de base
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 def f(x:float) -> float:
     """
     Calcule 2x
     """
     return 2*x
+
+data = pd.read_csv(fichier.csv) 
+
+
+## Partie Lise - la découverte des planètes
+
+
+
+
+## Partie Romain - caractéristiques mécaniques de la planète
+
+
+
+
+## Partie Ombline&Camille - caractéristiques physico-chimiques des planètes
