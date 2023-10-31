337c81485a4a35331974dfeb9b6a2c6606490ac1
Camille Prigent <camille.prigent@etu.minesparis.psl.eu>
2023-10-31 14:54:25 +0100

    mise en forme des données

--- code_1.py
+++ code_1.py
@@ -3,3 +3,38 @@ def f(x:float) -> float:
     Calcule 2x
     """
     return 2*x
+
+
+
+import pandas as pd
+df=pd.read_csv('fichier.csv')
+
+df.head()
+
+df = df[df.default_flag == 1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
